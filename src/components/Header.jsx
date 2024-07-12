93ea40ec02bfa1d10db1e5b1dd880f18de1ca75e
rbarakhvostov <rbarakhvostov@gmail.com>
2024-07-12 14:44:28 +0300

    implement team scene

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({ title, subtitle }) => {
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box mb="30px">
+    <Box mb="20px">
       <Typography variant="h2" color={colors.grey[100]} fontWeight="bold">
         {title}
       </Typography>
