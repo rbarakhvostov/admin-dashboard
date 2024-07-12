b7cb95da65516472b9d899f6e359be24d2815dac
rbarakhvostov <rbarakhvostov@gmail.com>
2024-07-12 15:50:21 +0300

    implement contact scene

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-// import Contacts from "./scenes/contacts";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/invoices";
 // import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
@@ -30,7 +30,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              <Route path="/contacts" element={<Contacts />} />
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
