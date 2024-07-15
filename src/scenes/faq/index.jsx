d29d99cbb826c58e2117475e0ebcb99c293f9feb
rbarakhvostov <rbarakhvostov@gmail.com>
2024-07-16 01:23:15 +0300

    implement bar chart

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -10,6 +10,7 @@ import { tokens } from "../../theme";
 const FAQ = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
