22c9dd9bb5077d0b8bcbda7720eff1fb185fb4fb
rbarakhvostov <rbarakhvostov@gmail.com>
2024-07-15 03:54:18 +0300

    implement calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
-
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
@@ -9,11 +8,11 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 // import Bar from "./scenes/bar";
 // import Pie from "./scenes/pie";
 // import Line from "./scenes/line";
 // import FAQ from "./scenes/faq";
-// import Calendar from "./scenes/calendar/calendar";
 // import Geography from "./scenes/geography";
 
 function App() {
@@ -33,11 +32,11 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
